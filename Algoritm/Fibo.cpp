6f6ece2127d1cf76727c756fa31c6e26070616e9
Anney218 <anneybegum0@gmail.com>
2024-12-05 14:28:13 +0600

    Add files via upload

---
+++ Algoritm/Fibo.cpp
@@ -0,0 +1,27 @@
+// Fibonacci Dynamic programming
+#include<iostream>
+using namespace std;
+long long m[100];
+long long fib(int n)
+{
+    if(n <=2)
+        return 1;
+    if(m[n] !=-1)
+        return m[n];
+    else
+    {
+        m[n] = fib(n-1)+fib(n-2);
+        return m[n];
+    }
+}
+    int main()
+    {
+        int n;
+        for(int i=0; i<100; i++)
+            m[i] = -1;
+        cout << "which Position: ";
+        cin >> n;
+        cout << fib(n);
+        return 0;
+    }
+
